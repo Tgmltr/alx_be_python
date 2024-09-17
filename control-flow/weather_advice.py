c8bdd1337f47d355f377f39126f0b9eb7f4090bc
Etaguemalter <eh.taguemalter@gmail.com>
2024-09-18 00:43:35 +0200

    modified the way i wrote the script to match the requirment for task

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,14 @@
 weather = input("What's the weather like today? (sunny/rainy/cold):")
-if weather == "sunny" :
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather == "rainy" :
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather  == "cold" :
+elif weather  == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else :
     print ("Sorry, I don't have recommendations for this weather.")
+
+#control-flow/weather_advice.py doesn't contain
+  #  elif weather == ['\"]rainy['\"]:\s*print\(\s*['\"]
     
     
