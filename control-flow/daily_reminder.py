00115817d15a18933a902dd5f938f8da77441204
Etaguemalter <eh.taguemalter@gmail.com>
2024-09-19 00:46:04 +0200

    modified the  file with a script for daily reminder task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
+while True:
+    task = input("Enter your task:")
+    priority = input("Priority (high/medium/low):").lower()
+    time_bound = input("Is it time-bound? (yes/no):").lower()
+    reminder=""
 
-task = input("Enter your task:")
-priority = input("Priority (high/medium/low):").lower()
-time_bound = input("Is it time-bound? (yes/no):").lower()
-reminder=""
-
-match priority :
-    case "high":
-        reminder = "reminder:"+task+" is a high priority "
-    case "medium":
-        reminder = "reminder:"+task+" is a medium priority "
-    case "low":
-        reminder = "reminder:"+task+" is a low priority "
-    case _: message = f"priority not defined "
-    
-if time_bound =="yes":
-    reminder += "that requires immediate attention today!"
-elif time_bound =="no":
-    reminder += "Consider completing it when you have free time."
-    
-print(reminder)
+    match priority :
+        case "high":
+            reminder = "reminder:" + task + " is a high priority task "
+        case "medium":
+            reminder = "reminder: "+task+" is a medium priority task "
+        case "low":
+            reminder = "reminder: " + task + " is a low priority task "
+        case _: message = f"priority not defined "
+        
+    if time_bound =="yes":
+        reminder += "that requires immediate attention today!"
+    elif time_bound =="no":
+        reminder += ". Consider completing it when you have free time."
+        
+    print(reminder)
