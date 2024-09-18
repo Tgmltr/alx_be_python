dd3f5d4f06613098828684344fc7512e9ee8bb64
Etaguemalter <eh.taguemalter@gmail.com>
2024-09-18 13:57:54 +0200

    modified the file multiplication_table with the script for multiplication

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,3 @@
-while True:
-    number = int(input("Enter a number to see its multiplication table:"))
-    for i in range (1,11):
-        print(number,'*', i ,'=', number * i )
+number = int(input("Enter a number to see its multiplication table:"))
+for i in range (1,11):
+    print(number,'*', i ,'=', number * i )
