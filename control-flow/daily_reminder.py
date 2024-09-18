ab748187eada9358e78ba6045b002890291d21e2
Etaguemalter <eh.taguemalter@gmail.com>
2024-09-19 00:55:14 +0200

    modified the  file with a script for daily reminder task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,20 +2,20 @@ while True:
     task = input("Enter your task:")
     priority = input("Priority (high/medium/low):").lower()
     time_bound = input("Is it time-bound? (yes/no):").lower()
-    reminder=""
+    Reminder=""
 
     match priority :
         case "high":
-            reminder = "reminder:" + task + " is a high priority task "
+            Reminder = "reminder:" + task + " is a high priority task "
         case "medium":
-            reminder = "reminder: "+task+" is a medium priority task "
+            Reminder = "reminder: "+task+" is a medium priority task "
         case "low":
-            reminder = "reminder: " + task + " is a low priority task "
+            Reminder = "reminder: " + task + " is a low priority task "
         case _: message = f"priority not defined "
         
     if time_bound =="yes":
-        reminder += "that requires immediate attention today!"
+        Reminder += "that requires immediate attention today!"
     elif time_bound =="no":
-        reminder += ". Consider completing it when you have free time."
+        Reminder += ". Consider completing it when you have free time."
         
-    print(reminder)
+    print('\n',Reminder)
