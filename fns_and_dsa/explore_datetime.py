5952a69a557feb0857ea7b8be778206faf941353
Etaguemalter <eh.taguemalter@gmail.com>
2024-10-01 00:14:03 +0200

    added the explore_datetime file to manipulate time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,13 @@
+from datetime import datetime, timedelta, date
+
+def display_current_datetime():
+    current_date = datetime.now()
+    formatted = current_date.strftime('%Y-%m-%d %H:%M:%S')
+    print(f"Current date and time: {formatted}")
+
+def calculate_future_date (number_of_days):
+    current_date = date.today()
+    future_date=current_date + timedelta(days=number_of_days)
+    print("Future date:", str(future_date))
+display_current_datetime()
+calculate_future_date(int(input("Enter the number of days to add to the current date: ")))
