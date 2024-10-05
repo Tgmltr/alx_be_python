5abf9bb9fe03009444f9d1a8611b611074d83f1f
Etaguemalter <eh.taguemalter@gmail.com>
2024-10-05 16:46:32 +0200

    created the file main-0.py with it s script, and created the classe bankAccount with its methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, account_ballance):
+        self.account_ballance = account_ballance
+        #self.account_ballance=0
+
+    def deposit(self,amount):
+        self.account_ballance += amount
+
+    def withdraw(self,amount):
+        if amount<=self.account_ballance:
+            self.account_ballance -= amount
+            return True
+        else: return False
+
+    def display_balance(self):
+        print(f"Your ballance is: {self.account_ballance} DH")
