f1070d6de9fa84f558b2f952581eb6262f5ee919
Etaguemalter <eh.taguemalter@gmail.com>
2024-10-12 15:37:47 +0200

    modified the positions of the points to match task requirment (2)

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,14 +6,14 @@ class Book:
         return f"Book: {self.title} by {self.author}"
         
 
-class EBook (Book) :
+class EBook(Book):
     def __init__(self,title, author, file_size):
         super().__init__(title,author)
         self.file_size=file_size
     def __repr__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-class PrintBook (Book) :
+class PrintBook(Book):
     def __init__(self,title,author,page_count):
         super().__init__(title,author)
         self.page_count=page_count
